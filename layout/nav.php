61dcc3efaf55dab53421221803656b425d757750
simon mbugua <smbugua11@gmail.com>
2018-04-19 16:49:28 +0300

    menu change

--- layout/nav.php
+++ layout/nav.php
@@ -337,7 +337,7 @@ echo <<<Headerfunction
 								<a href="../parameters/view_parameters.php">Config Parameters</a>
 							</li>
 							<li>
-								<a href="../market_parameters/market_parameters.php">Market Configurations</a>
+								<a href="../view_market_parameters/market_parameters.php">Market Configurations</a>
 							</li>
 							
 						</ul>
