160d040a0e2f94ccbcec75f3199c064d3c717153
simon mbugua <smbugua11@gmail.com>
2018-05-03 11:42:08 +0300

    ok

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 require_once('app/includes/auth.php');
 if(isset($_SESSION['loggedin'])==true){
-header("Location: app/access/logout");
+header("Location: app/access/logout.php");
 }
 elseif(isset($_SESSION['loggedin'])==false){
-echo "<script>location.replace('app/access/index')</script>";
+echo "<script>location.replace('app/access/index.php')</script>";
 }
 
 
